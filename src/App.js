fc0a5cd7aaf8778cd875ecfb47f23adb7e1c2288
Gireeshkumar Ediga <egireeshkumar99@gmail.com>
2021-12-25 21:15:56 +0530

    webpack

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+      <h2>my first react assignment</h2>
+      </center>
+      {/* <img src="./logo192.png"/>
+      <img src="./logo512.png" /> */}
+      <img src="./download.jfif" />
+      <img src="./images.jfif" />
+    </div>
+  );
+}
+
+export default App;
